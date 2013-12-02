6789eb6d8d21f11786c02ab55fc8e2864ff125c6
cJustin <cJustin@ThePinkness7>
2013-12-02 10:52:45 -0500

    Added class

---
+++ Retry/src/Adjacency.java
@@ -0,0 +1,17 @@
+
+public class Adjacency {
+
+	int node1;
+	int node2;
+	int useCount;	
+	
+	
+	
+	public Adjacency(int node1,int node2, int useCount){
+		this.node1=node1;
+		this.node2=node2;
+		this.useCount=useCount;
+	}
+	
+	
+}
